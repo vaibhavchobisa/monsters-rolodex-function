ff659fe7dab7464a2c1b2a6216b19ab983177a06
Vaibhav Chobisa <vaibhav.chobisa@gmail.com>
2022-06-12 00:51:20 +0530

    adding genesis files

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,15 @@
+// import { Component } from 'react';
+import './card.styles.css';
+
+const Card = ({ monster }) => {
+        const { id, name, email } = monster;
+        return (
+            <div className='card-container' key={id}>
+                <img src={`https://robohash.org/${id}/?set=set2&size=180x180`} alt={`monster ${name}`} />
+                <h2 className='h2'>{name}</h2>
+                <p className='email'>{email.toLocaleLowerCase()}</p>
+            </div>
+        )
+    }
+
+export default Card;
